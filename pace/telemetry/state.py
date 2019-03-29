53784c96d738f5df4f2021450de740e358f8237c
Jordan Blackadar <jordan.blackadar@outlook.com>
2019-03-29 17:17:18 -0400

    Completed first iteration of channel hopper

--- pace/telemetry/state.py
+++ pace/telemetry/state.py
@@ -2,6 +2,7 @@
 Handles setting up the Raspberry Pi for Monitoring
 """
 import subprocess
+import threading
 
 
 def monitor(adapter):
@@ -23,9 +24,42 @@ def unmonitor(adapter):
     subprocess.call(['ifconfig', adapter, 'up'])
 
 
-def hop():
-    """
-    Enter a band hopping loop
-    :return:
-    """
-    pass
+class Hopper:
+
+    def __init__(self, device):
+        self.device = device
+        self.thread = None
+        self.__stop = False
+
+    def hop(self):
+        """
+        Enter a band hopping loop
+        :return:
+        """
+        self.thread = threading.Thread(target=self.__hopper)
+        self.thread.start()
+
+    def __hopper(self):
+        """
+        Thread worker
+        :return:
+        """
+        import time
+        while not self.__stop:
+            subprocess.call('iwconfig', self.device, 'channel 1')
+            if not self.__stop:
+                time.sleep(3)
+            subprocess.call('iwconfig', self.device, 'channel 6')
+            if not self.__stop:
+                time.sleep(3)
+            subprocess.call('iwconfig', self.device, 'channel 11')
+            if not self.__stop:
+                time.sleep(3)
+
+    def stop(self):
+        """
+        Stop! Data time
+        :return:
+        """
+        self.__stop = True
+        self.thread = None
