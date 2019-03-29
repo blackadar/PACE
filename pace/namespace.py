fb46a704618e2a5a00c74508264bb229684fb5fa
Jordan Blackadar <jordan.blackadar@outlook.com>
2019-03-29 16:16:07 -0400

    Update namespace.py

--- pace/namespace.py
+++ pace/namespace.py
@@ -3,4 +3,5 @@ Namespace Variables
 Intended to define constants.
 """
 
-RECENT_PERSIST = 15  # Time in minutes to ignore MAC re-broacast
+RECENT_PERSIST = 15  # Time in minutes to consider a device as already present
+DATA_WRITE = 1  # Time interval in minutes to write dataframe to a file
