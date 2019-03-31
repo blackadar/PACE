834314045de57ea00c0ea20813c564af995997dc
Jordan Blackadar <jordan.blackadar@outlook.com>
2019-03-31 17:43:15 -0400

    Fixed Threading set access issue Added logging for unhandled exceptions to log file Optimized imports

--- pace/cli.py
+++ pace/cli.py
@@ -1,7 +1,6 @@
 """
 Coordinates the operation of a RasPi PACE machine
 """
-import datetime
 import logging
 import signal
 import sys
@@ -20,6 +19,14 @@ def main():
 
     # Start the Logger
     logging.basicConfig(filename='pace.log', level=logging.DEBUG, format='%(levelname)s:  %(message)s')
+
+    def handle_exception(exc_type, exc_value, exc_traceback):
+        if issubclass(exc_type, KeyboardInterrupt):
+            sys.__excepthook__(exc_type, exc_value, exc_traceback)
+            return
+        logging.error("Uncaught Exception", exc_info=(exc_type, exc_value, exc_traceback))
+
+    sys.excepthook = handle_exception
 
     # Initialize device
     state.monitor(namespace.ADAPTER)
@@ -42,26 +49,7 @@ def main():
 
     while True:
         time.sleep(5)
-        print_current(area.interval_total, area.interval_devices, area.last_interval, area.interval_ssids)
-
-
-def print_current(tot: int, interval: set, last: datetime.datetime, ssids: dict):
-    logging.debug("last dump: " + str(last))
-    logging.debug("total probes: " + str(tot))
-    logging.debug("mac pool: ")
-    for item in interval:
-        logging.debug(" " + item)
-    logging.debug("ssid requests: ")
-    for key, val in ssids.items():
-        logging.debug(" " + str(key) + ": " + str(val))
-
-    logging.debug("---------------------------")
-
-
-def print_telemetry(telemetry: dict):
-    for key, val in telemetry.items():
-        print(key, val)
-    print()
+        area.log()
 
 
 if __name__ == "__main__":
--- pace/telemetry/congestion.py
+++ pace/telemetry/congestion.py
@@ -10,6 +10,7 @@ import time
 
 from scapy.all import Dot11, sniff
 
+import pace.analysis.read
 import pace.namespace as namespace
 
 
@@ -23,6 +24,8 @@ class Area:
         self.last_interval = datetime.datetime.now()
         self.interval_devices = set()  # Safe to ignore duplicates within interval
         self.interval_ssids = {}
+        self.interval_devices_lock = threading.RLock()
+        self.interval_ssids_lock = threading.RLock()
         self.interval_total = 0
         self.interface = interface
         self.interval_thread = None
@@ -32,33 +35,37 @@ class Area:
     def register(self, mac, info):
         """
         Registers a MAC as seen in the area
+        :param info:
         :param mac:
         :return:
         """
         self.interval_total += 1
-        self.interval_devices.add(mac)
+        with self.interval_devices_lock:
+            self.interval_devices.add(mac)
         if info != '':
-            if info not in self.interval_ssids.keys():
-                self.interval_ssids[info] = 1
-            else:
-                self.interval_ssids[info] += 1
+            with self.interval_ssids_lock:
+                if info not in self.interval_ssids.keys():
+                    self.interval_ssids[info] = 1
+                else:
+                    self.interval_ssids[info] += 1
 
     def new_interval(self):
         """
         Remove old entries
         :return:
         """
-        # Attempt to prevent memory leak
-        del self.interval_devices
-        del self.interval_ssids
-        del self.interval_total
-        del self.last_interval
+        with self.interval_devices_lock and self.interval_ssids_lock:
+            # Attempt to prevent memory leak
+            del self.interval_devices
+            del self.interval_ssids
+            del self.interval_total
+            del self.last_interval
 
-        self.interval_devices = set()
-        self.interval_ssids = {}
-        self.interval_total = 0
-        self.last_interval = datetime.datetime.now()
-        logging.debug("Starting a new time frame at " + str(self.last_interval))
+            self.interval_devices = set()
+            self.interval_ssids = {}
+            self.interval_total = 0
+            self.last_interval = datetime.datetime.now()
+            logging.debug("Starting a new time frame at " + str(self.last_interval))
 
     def monitor(self):
         """
@@ -90,7 +97,7 @@ class Area:
         logging.debug("Initializing Scapy on " + str(self.interface))
         sniff(iface=self.interface, prn=self.handle_event, stop_filter=self.__should_stop, store=0)
 
-    def __should_stop(self, garbage):
+    def __should_stop(self, __):
         """
         Returns True if the sniffer should stop, instead of reading next packet.
         :return:
@@ -107,11 +114,11 @@ class Area:
 
     def handle_event(self, pkt):
         """
-            Handle a packet from scapy
-            I-Spy some useful data
-            :param pkt: Packet from sniffer
-            :return:
-            """
+        Handle a packet from scapy
+        I-Spy some useful data
+        :param pkt: Packet from sniffer
+        :return:
+        """
         if not pkt.haslayer(Dot11):
             return  # Don't care
         if pkt.type == 0 and pkt.subtype == 4:  # IEEE 802.11 Management, Re-Association Request
@@ -131,9 +138,24 @@ class Area:
         """
         name = namespace.DATA_PATH + time.strftime("%Y%m%d_%H%M%S.pkl")
         logging.debug("Writing to file " + name)
-        telemetry = {'Interval': self.last_interval, 'Devices': self.interval_devices,
-                     'Total Probes': self.interval_total, 'SSID Requests': self.interval_ssids}
-        import pace.analysis.read
+        with self.interval_devices_lock and self.interval_ssids_lock:
+            telemetry = {'Interval': self.last_interval, 'Devices': self.interval_devices,
+                         'Total Probes': self.interval_total, 'SSID Requests': self.interval_ssids}
         pace.analysis.read.check_create_data()
         with open(name, "wb") as pkl:
             pickle.dump(telemetry, pkl)
+
+    def log(self):
+
+        logging.debug("last dump: " + str(self.last_interval))
+        logging.debug("total probes: " + str(self.interval_total))
+        logging.debug("mac pool: ")
+        with self.interval_devices_lock:
+            for item in self.interval_devices:
+                logging.debug(" " + item)
+        logging.debug("ssid requests: ")
+        with self.interval_ssids_lock:
+            for key, val in self.interval_ssids.items():
+                logging.debug(" " + str(key) + ": " + str(val))
+
+        logging.debug("---------------------------")
